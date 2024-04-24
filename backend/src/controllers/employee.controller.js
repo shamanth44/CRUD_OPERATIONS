a679e70ce66c4980295c6f5daff87441e87931be
Shamanth Ganiger <shamanthganiger@gmail.com>
2024-04-24 23:39:41 +0530

    writing controllers to create employee, get employees, update employee, delete employee

--- backend/src/controllers/employee.controller.js
+++ backend/src/controllers/employee.controller.js
@@ -5,55 +5,138 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const createEmployee = asyncHandler(async (req, res) => {
-try {
-        const { uniqueId, name, email, mobileNo, designation, gender, course } = req.body;
+  // try {
+  const { uniqueId, name, email, mobileNo, designation, gender, course } =
+    req.body;
+
+  if (
+    [uniqueId, name, email, mobileNo, designation, gender, course].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedEmployee = await Employee.findOne({
+    $or: [{ uniqueId }, { name }, { email }, { mobileNo }],
+  });
+
+  if (existedEmployee) {
+    throw new ApiError(409, "Employee already existed");
+  }
+
+  const imageLocalPath = req.files?.image[0].path;
+
+  if (!imageLocalPath) {
+    throw new ApiError(400, "Image is required");
+  }
+
+  const image = await uploadOnCloudinary(imageLocalPath);
+
+  if (!image) {
+    throw new ApiError(400, "Image is required");
+  }
+
+  const employee = await Employee.create({
+    uniqueId,
+    name,
+    email,
+    mobileNo,
+    designation,
+    gender,
+    course,
+    image: image.url,
+  });
+
+  if (!employee) {
+    throw new ApiError(500, "Something went wrong while creating employee");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, employee, "Employee created successfully"));
+  // } catch (error) {
+  //     throw new ApiError(500, "Something went wrong");
+  // }
+});
+
+const getAllEmployees = asyncHandler(async (req, res) => {
+
+    const allEmployees = await Employee.find()
+
+    if (!allEmployees) {
+        throw new ApiError (500, "Something went wrong while fetching employees")
+    }
+
+    return res.status(201)
+    .json(new ApiResponse(200, {allEmployees}, "Employee fetched successfully"))
     
-        if([uniqueId, name, email, mobileNo, designation, gender, course].some((field) => field?.trim() === "")) {
-            throw new ApiError(400, "All fields are required");
-        }
-    
-        const existedEmployee = await Employee.findOne({
-            $or: [ {uniqueId}, {name}, {email}],
-        });
-    
-        if(existedEmployee) {
-            throw new ApiError(409, "Employee already existed");
-        }
-    
-        const imageLocalPath = req.files?.image[0].path;
-    
-        if(!imageLocalPath) {
-            throw new ApiError(400, "Image is required");
-        }
-    
-        const image = await uploadOnCloudinary(imageLocalPath);
-    
-        if(!image) {
-            throw new ApiError(400, "Image is required");
-        }
-    
-        const employee = await Employee.create({
-            uniqueId,
-            name,
-            email,
-            mobileNo,
-            designation,
-            gender,
-            course,
-            image: image.url,
-        })
-    
-    
-        if(!employee) {
-            throw new ApiError(500, "Something went wrong while creating employee")
-        }
-    
-        return res
-        .status(201)
-        .json(new ApiResponse(200, employee, "Employee created successfully"))
-} catch (error) {
-    throw new ApiError(500, "Something went wrong");
-}
 })
 
-export{ createEmployee } ; 
+const updateEmployee = asyncHandler(async (req, res) => {
+  const { uniqueId, name, email, mobileNo, designation, gender, course } =
+    req.body;
+  const id = req.params.id;
+
+  if (
+    [uniqueId, name, email, mobileNo, designation, gender, course].some(
+      (field) => field?.trim() === ""
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const imageLocalPath = req.files?.image[0].path;
+
+  if (!imageLocalPath) {
+    throw new ApiError(400, "Image is required");
+  }
+
+  const image = await uploadOnCloudinary(imageLocalPath);
+
+  if (!image) {
+    throw new ApiError(400, "Image is required");
+  }
+
+  const updateEmployee = await Employee.findByIdAndUpdate(
+    { _id: id },
+    {
+      uniqueId,
+      name,
+      email,
+      mobileNo,
+      designation,
+      gender,
+      course,
+      image: image.url,
+    }
+  );
+
+  if (!updateEmployee) {
+    throw new ApiError(500, "Something went wrong while updating employee");
+  }
+
+  const updatedEmployee = await Employee.findById(updateEmployee._id);
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(200, updatedEmployee, "Employee updated successfully")
+    );
+});
+
+const deleteEmployee = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+
+  const deleteEmployee = await Employee.findByIdAndDelete({ _id: id });
+
+  if (!deleteEmployee) {
+    throw new ApiError(500, "Something went wrong while deleting employee");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, "Employee deleted successfully"));
+});
+
+export { createEmployee, updateEmployee, deleteEmployee, getAllEmployees };
--- backend/src/routes/employee.routes.js
+++ backend/src/routes/employee.routes.js
@@ -1,15 +1,39 @@
 import { Router } from "express";
-import { loginAdmin, logoutAdmin, refreshAccessToken, registerAdmin } from "../controllers/admin.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { createEmployee } from "../controllers/employee.controller.js";
+import { createEmployee, deleteEmployee, getAllEmployees, updateEmployee } from "../controllers/employee.controller.js";
 
 const router = Router();
 
-
 router
   .route("/create-employee")
-  .post(verifyJwt, upload.fields([{ name: "image", maxCount: 1 }]), createEmployee);
+  .post(
+    verifyJwt,
+    upload.fields([{ name: "image", maxCount: 1 }]),
+    createEmployee
+  );
 
+router
+  .route("/update-employee/:id")
+  .put(
+    verifyJwt,
+    upload.fields([{ name: "image", maxCount: 1 }]),
+    updateEmployee
+  );
+
+  router
+  .route("/delete-employee/:id")
+  .delete(
+    verifyJwt,
+    deleteEmployee
+  );
+
+  router
+  .route("/get-employees")
+  .get(
+    verifyJwt,
+    getAllEmployees
+  );
+  
 
 export default router;
